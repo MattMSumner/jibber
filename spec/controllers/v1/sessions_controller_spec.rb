07e086f59ce1e8bce4f8a05a8b129e17d14f83ed
Matthew Sumner <matt.m.sumner@gmail.com>
2015-02-10 17:27:33 -0500

    Sessions#create now accepts a token
    This allows clients to "fetch" a session.

--- spec/controllers/v1/sessions_controller_spec.rb
+++ spec/controllers/v1/sessions_controller_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe V1::SessionsController do
-  describe "#new" do
+  describe "#create" do
     context "github-auth-code present" do
       it "authenticates via github" do
         user = double(:user)
@@ -17,6 +17,25 @@ describe V1::SessionsController do
         post :create, 'github-auth-code' => 'valid_code'
 
         expect(response).to have_http_status(:created)
+      end
+    end
+
+    context "token present" do
+      it "fetches the user" do
+        user = double(:user)
+        allow(User).to receive(:find_by).with(token: "token").and_return(user)
+
+        post :create, token: :token
+
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context "invalid params present" do
+      it "returns bad_request" do
+        post :create
+
+        expect(response).to have_http_status(:bad_request)
       end
     end
   end
